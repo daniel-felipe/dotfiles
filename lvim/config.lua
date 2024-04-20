52b612b16ebaa6d78f052bc2295667ce3775dc46
Daniel Felipe <daniell.felipe.2017@gmail.com>
2024-04-20 11:04:18 -0300

    fix: config

--- lvim/config.lua
+++ lvim/config.lua
@@ -109,22 +109,19 @@ vim.opt.relativenumber = true
 
 vim.opt.title = true
 vim.opt.mouse = 'a'
-vim.opt.termguicolors = true
-vim.opt.spell = true
-
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
+
 vim.opt.list = true
 vim.opt.listchars = { tab = '▸ ', trail = '·' }
-
 vim.opt.splitbelow = true
 vim.opt.splitright = true
+
 vim.opt.scrolloff = 8
 vim.opt.sidescrolloff = 8
-
 vim.opt.confirm = true
 vim.opt.signcolumn = 'yes:2'
+
 vim.opt.undofile = true
 vim.opt.backup = true
-
 vim.opt.backupdir:remove('.')
