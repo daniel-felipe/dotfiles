efe7cd54d8625a35d6453f1c0924c22f7a1d198b
Daniel Felipe <daniell.felipe.2017@gmail.com>
2024-09-21 15:38:56 -0300

    wip

--- setup/arch/setup.sh
+++ setup/arch/setup.sh
@@ -13,10 +13,10 @@ sudo pacman -Syyu --noconfirm
 sudo pacman -S --noconfirm yay 
 
 
-source "$DOTFILES/setup/dev.sh"
-source "$DOTFILES/setup/media.sh"
-source "$DOTFILES/setup/fonts.sh"
-source "$DOTFILES/setup/sys.sh"
+source "$DOTFILES/setup/arch/dev.sh"
+source "$DOTFILES/setup/arch/media.sh"
+source "$DOTFILES/setup/arch/fonts.sh"
+source "$DOTFILES/setup/arch/sys.sh"
 
 
 # ----------------------------------------------------
