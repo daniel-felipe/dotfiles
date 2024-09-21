95acbbb42986a3ef444f020b458fbfa0eb9544f6
Daniel Felipe <daniell.felipe.2017@gmail.com>
2024-09-21 15:47:05 -0300

    wip

--- setup/arch/dev.sh
+++ setup/arch/dev.sh
@@ -12,6 +12,10 @@ yay --noconfirm --needed base-devel git lazygit neovim \
 # ----------------------------------------------------
 # Install Astronvim
 # ----------------------------------------------------
+mv ~/.config/nvim ~/.config/nvim.bak
+mv ~/.local/share/nvim ~/.local/share/nvim.bak
+mv ~/.local/state/nvim ~/.local/state/nvim.bak
+mv ~/.cache/nvim ~/.cache/nvim.bak
 git clone --depth 1 https://github.com/AstroNvim/template ~/.config/nvim
 rm -rf ~/.config/nvim/.git
 
